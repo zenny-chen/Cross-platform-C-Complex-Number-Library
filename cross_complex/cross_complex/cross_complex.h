b5d660c8d245ef335769542c1afc4c2b70da2384
Zenny Chen <zenny_chen@sina.com>
2022-10-28 23:32:09 +0800

    Add files via upload

---
+++ cross_complex/cross_complex/cross_complex.h
@@ -0,0 +1,242 @@
+﻿#ifndef CROSS_PLATFORM_COMPLEX_LIB
+#define CROSS_PLATFORM_COMPLEX_LIB
+
+#include <complex.h>
+
+#ifdef _MSC_VER
+// MSVC compiler：
+#define CMPLX       _DCOMPLEX_
+#define CMPLXF      _FCOMPLEX_
+#define CMPLXL      _LCOMPLEX_
+
+// Complex Add
+static inline _Dcomplex cadd(_Dcomplex a, _Dcomplex b)
+{
+    return CMPLX(creal(a) + creal(b),
+        cimag(a) + cimag(b));
+}
+
+static inline _Fcomplex caddf(_Fcomplex a, _Fcomplex b)
+{
+    return CMPLXF(crealf(a) + crealf(b),
+        cimagf(a) + cimagf(b));
+}
+
+static inline _Lcomplex caddl(_Lcomplex a, _Lcomplex b)
+{
+    return CMPLXL(creall(a) + creall(b),
+        cimagl(a) + cimagl(b));
+}
+
+// Complex Subtraction
+static inline _Dcomplex csub(_Dcomplex a, _Dcomplex b)
+{
+    return CMPLX(creal(a) - creal(b),
+        cimag(a) - cimag(b));
+}
+
+static inline _Fcomplex csubf(_Fcomplex a, _Fcomplex b)
+{
+    return CMPLXF(crealf(a) - crealf(b),
+        cimagf(a) - cimagf(b));
+}
+
+static inline _Lcomplex csubl(_Lcomplex a, _Lcomplex b)
+{
+    return CMPLXL(creall(a) - creall(b),
+        cimagl(a) - cimagl(b));
+}
+
+// Complex Multiplication (a complex with a complex)
+static inline _Dcomplex cmulcc(_Dcomplex a, _Dcomplex b)
+{
+    return _Cmulcc(a, b);
+}
+
+static inline _Fcomplex cmulccf(_Fcomplex a, _Fcomplex b)
+{
+    return _FCmulcc(a, b);
+}
+
+static inline _Lcomplex cmulccl(_Lcomplex a, _Lcomplex b)
+{
+    return _LCmulcc(a, b);
+}
+
+// Complex Multiplication (a complex with a real floating-point number)
+static inline _Dcomplex cmulcr(_Dcomplex a, double b)
+{
+    return _Cmulcr(a, b);
+}
+
+static inline _Fcomplex cmulcrf(_Fcomplex a, float b)
+{
+    return _FCmulcr(a, b);
+}
+
+static inline _Lcomplex cmulcrl(_Lcomplex a, long double b)
+{
+    return _LCmulcr(a, b);
+}
+
+// Complex Division (a complex with a complex)
+static inline _Dcomplex cdivcc(_Dcomplex a, _Dcomplex b)
+{
+    const double denominator = creal(b) * creal(b) + cimag(b) * cimag(b);
+
+    double real = creal(a) * creal(b) + cimag(a) * cimag(b);
+    real /= denominator;
+
+    double imag = cimag(a) * creal(b) - creal(a) * cimag(b);
+    imag /= denominator;
+
+    return CMPLX(real, imag);
+}
+
+static inline _Fcomplex cdivccf(_Fcomplex a, _Fcomplex b)
+{
+    const float denominator = crealf(b) * crealf(b) + cimagf(b) * cimagf(b);
+
+    float real = crealf(a) * crealf(b) + cimagf(a) * cimagf(b);
+    real /= denominator;
+
+    float imag = cimagf(a) * crealf(b) - crealf(a) * cimagf(b);
+    imag /= denominator;
+
+    return CMPLXF(real, imag);
+}
+
+static inline _Lcomplex cdivccl(_Lcomplex a, _Lcomplex b)
+{
+    const long double denominator = creall(b) * creall(b) + cimagl(b) * cimagl(b);
+
+    long double real = creall(a) * creall(b) + cimagl(a) * cimagl(b);
+    real /= denominator;
+
+    long double imag = cimagl(a) * creall(b) - creall(a) * cimagl(b);
+    imag /= denominator;
+
+    return CMPLXL(real, imag);
+}
+
+// Complex Division (a complex with a real floating-point number)
+static inline _Dcomplex cdivcr(_Dcomplex a, double b)
+{
+    return _Cmulcr(a, 1.0 / b);
+}
+
+static inline _Fcomplex cdivcrf(_Fcomplex a, float b)
+{
+    return _FCmulcr(a, 1.0f / b);
+}
+
+static inline _Lcomplex cdivcrl(_Lcomplex a, long double b)
+{
+    return _LCmulcr(a, 1.0L / b);
+}
+
+#else
+// Other compilers that support C11 <complex.h>
+typedef double complex          _Dcomplex;
+typedef float complex           _Fcomplex;
+typedef long double complex     _Lcomplex;
+
+// Complex Add
+static inline _Dcomplex cadd(_Dcomplex a, _Dcomplex b)
+{
+    return a + b;
+}
+
+static inline _Fcomplex caddf(_Fcomplex a, _Fcomplex b)
+{
+    return a + b;
+}
+
+static inline _Lcomplex caddl(_Lcomplex a, _Lcomplex b)
+{
+    return a + b;
+}
+
+// Complex Subtraction
+static inline _Dcomplex csub(_Dcomplex a, _Dcomplex b)
+{
+    return a - b;
+}
+
+static inline _Fcomplex csubf(_Fcomplex a, _Fcomplex b)
+{
+    return a - b;
+}
+
+static inline _Lcomplex csubl(_Lcomplex a, _Lcomplex b)
+{
+    return a - b;
+}
+
+// Complex Multiplication (a complex with a complex)
+static inline _Dcomplex cmulcc(_Dcomplex a, _Dcomplex b)
+{
+    return a * b;
+}
+
+static inline _Fcomplex cmulccf(_Fcomplex a, _Fcomplex b)
+{
+    return a * b;
+}
+
+static inline _Lcomplex cmulccl(_Lcomplex a, _Lcomplex b)
+{
+    return a * b;
+}
+
+// Complex Multiplication (a complex with a real floating-point number)
+static inline _Dcomplex cmulcr(_Dcomplex a, double b)
+{
+    return a * b;
+}
+
+static inline _Fcomplex cmulcrf(_Fcomplex a, float b)
+{
+    return a * b;
+}
+
+static inline _Lcomplex cmulcrl(_Lcomplex a, long double b)
+{
+    return a * b;
+}
+
+// Complex Division (a complex with a complex)
+static inline _Dcomplex cdivcc(_Dcomplex a, _Dcomplex b)
+{
+    return a / b;
+}
+
+static inline _Fcomplex cdivccf(_Fcomplex a, _Fcomplex b)
+{
+    return a / b;
+}
+
+static inline _Lcomplex cdivccl(_Lcomplex a, _Lcomplex b)
+{
+    return a / b;
+}
+
+// Complex Division (a complex with a real floating-point number)
+static inline _Dcomplex cdivcr(_Dcomplex a, double b)
+{
+    return a / b;
+}
+
+static inline _Fcomplex cdivcrf(_Fcomplex a, float b)
+{
+    return a / b;
+}
+
+static inline _Lcomplex cdivcrl(_Lcomplex a, long double b)
+{
+    return a / b;
+}
+
+#endif // _MSC_VER
+
+#endif
---
+++ cross_complex/cross_complex/main.c
@@ -0,0 +1,60 @@
+﻿#if __ANDROID__
+#include <jni.h>
+#include <syslog.h>
+#endif
+
+#include <stdio.h>
+#include "cross_complex.h"
+
+int main(void)
+{
+    _Dcomplex dcomp = CMPLX(1.5, -2.5);
+    _Fcomplex fcomp = CMPLXF(-0.5f, 1.25f);
+    _Lcomplex lcomp = CMPLXL(100.25L, 300.75L);
+
+    dcomp = cadd(dcomp, CMPLX(1.5, 0.5));
+    fcomp = caddf(fcomp, CMPLXF(1.5f, 0.75f));
+    lcomp = caddl(lcomp, CMPLXL(1.75L, 10.25L));
+
+    dcomp = csub(dcomp, CMPLX(1.5, 0.5));
+    fcomp = csubf(fcomp, CMPLXF(1.5f, 0.75f));
+    lcomp = csubl(lcomp, CMPLXL(1.75L, 10.25L));
+
+    dcomp = cmulcc(dcomp, CMPLX(0.2, 10.0));
+    fcomp = cmulccf(fcomp, CMPLXF(1.5f, 20.0f));
+    lcomp = cmulccl(lcomp, CMPLXL(0.8L, 0.4L));
+
+    dcomp = cmulcr(dcomp, 10.0);
+    fcomp = cmulcrf(fcomp, 10.0f);
+    lcomp = cmulcrl(lcomp, 10.0L);
+
+    dcomp = cdivcc(dcomp, CMPLX(0.2, 10.0));
+    fcomp = cdivccf(fcomp, CMPLXF(1.5f, 20.0f));
+    lcomp = cdivccl(lcomp, CMPLXL(0.8L, 0.4L));
+
+    dcomp = cdivcr(dcomp, 10.0);
+    fcomp = cdivcrf(fcomp, 10.0f);
+    lcomp = cdivcrl(lcomp, 10.0L);
+
+    // The followings are contained in the <complex.h> header...
+
+    // conjugate 
+    dcomp = conj(dcomp);
+    fcomp = conjf(fcomp);
+    lcomp = conjl(lcomp);
+
+    // absolute value
+    const double d = cabs(dcomp);
+    const float f = cabsf(fcomp);
+    const long double ld = cabsl(lcomp);
+
+#if __ANDROID__
+    syslog(LOG_INFO, "dcomp = (%f + %fi), d = %f\n", creal(dcomp), cimag(dcomp), d);
+    syslog(LOG_INFO, "fcomp = (%f + %fi), f = %f\n", crealf(fcomp), cimagf(fcomp), f);
+    syslog(LOG_INFO, "lcomp = (%Lf + %Lfi), ld = %Lf\n", creall(lcomp), cimagl(lcomp), ld);
+#else
+    printf("dcomp = (%f + %fi), d = %f\n", creal(dcomp), cimag(dcomp), d);
+    printf("fcomp = (%f + %fi), f = %f\n", crealf(fcomp), cimagf(fcomp), f);
+    printf("lcomp = (%Lf + %Lfi), ld = %Lf\n", creall(lcomp), cimagl(lcomp), ld);
+#endif
+}
